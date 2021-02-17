543711aa1e0fb093790bf5781bc0582d1d1c9cae
lopez05656 <55638449+lopez05656@users.noreply.github.com>
2021-02-17 17:54:28 -0500

    Update BoardConfig-common.mk

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -26,11 +26,6 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := kryo385
-TARGET_KERNEL_SOURCE := device/google/crosshatch-kernel
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := b1c1_defconfig
-TARGET_KERNEL_ARCH := arm64
-
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
@@ -42,6 +37,12 @@ TARGET_BOARD_COMMON_PATH := device/google/crosshatch/sdm845
 
 BUILD_BROKEN_DUP_RULES := true
 
+TARGET_KERNEL_SOURCE := device/google/crosshatch-kernel
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := b1c1_defconfig
+TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_IMAGE_NAME += Image.lz4
+
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
@@ -51,7 +52,6 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
-BOARD_KERNEL_IMAGE_NAME += Image.lz4
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
