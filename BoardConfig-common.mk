e0c599f5192437b570beafbf6be927c0da4f5acd
lopez05656 <55638449+lopez05656@users.noreply.github.com>
2021-02-18 09:27:52 -0500

    Update BoardConfig-common.mk

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -47,6 +47,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
+TARGET_PREBUILT_KERNEL += device/google/crosshatch-kernel/Image.lz4
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/google/bluecross
