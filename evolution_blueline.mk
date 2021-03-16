b06ef7ba6eba88fe5cf436a8874de87269c8455f
lopez05656 <55638449+lopez05656@users.noreply.github.com>
2021-03-16 10:05:08 -0400

    Update DT

--- evolution_blueline.mk
+++ evolution_blueline.mk
@@ -27,10 +27,6 @@ $(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
 PRODUCT_NAME := evolution_blueline
 PRODUCT_DEVICE := blueline
 PRODUCT_BRAND := google
--- evolution_crosshatch.mk
+++ evolution_crosshatch.mk
@@ -27,9 +27,6 @@ $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 PRODUCT_NAME := evolution_crosshatch
 PRODUCT_DEVICE := crosshatch
 PRODUCT_BRAND := google
