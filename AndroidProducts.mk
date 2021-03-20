8d8369aa1f86dbe4dc26b10eafd9abdd9fc1207d
lopez05656 <55638449+lopez05656@users.noreply.github.com>
2021-03-20 00:33:01 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -19,7 +19,3 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_blueline.mk \
     $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
     $(LOCAL_DIR)/aosp_blueline_hwasan.mk
-
-#COMMON_LUNCH_CHOICES := \
-#    aosp_crosshatch-userdebug \
-#    aosp_blueline-userdebug
