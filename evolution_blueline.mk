95e0bca9b388807792880eac020b6a4b1500e99b
lopez05656 <55638449+lopez05656@users.noreply.github.com>
2021-04-08 14:25:24 -0400

    Replace Props and move Maintainer string to an Overlay

--- evolution_blueline.mk
+++ evolution_blueline.mk
@@ -38,15 +38,8 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_GAPPS_ARCH := arm64
-EVO_MAINTAINER := lopez05656
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="blueline" \
-    PRIVATE_BUILD_DESC="blueline-user 11 RQ2A.210405.005 7181113 release-keys"
-
-BUILD_FINGERPRINT := "google/blueline/blueline:11/RQ2A.210405.005/7181113:user/release-keys"
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/blueline/blueline:11/RQ2A.210405.005/7181113:user/release-keys
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
