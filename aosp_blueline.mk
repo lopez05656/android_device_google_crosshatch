bb1d323c192aa16d567f15b6e019f5aad2973120
Lop <55638449+lopez05656@users.noreply.github.com>
2021-07-02 21:09:24 -0400

    Update aosp_blueline.mk

--- aosp_blueline.mk
+++ aosp_blueline.mk
@@ -19,7 +19,6 @@
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Enable mainline checking
 # TODO(b/138706293): enable Enable mainline checking later
